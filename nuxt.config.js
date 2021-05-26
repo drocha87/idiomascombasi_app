f76fb21426fca932a0e9e6ab93180af80f302b80
Diego Rocha <dlmrocha87@gmail.com>
2021-05-26 15:36:01 -0300

    remove global error handler for now

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: 'plugins/globalErrorHandler.ts', mode: 'client' },
     { src: '~/plugins/vue-markdown.js', mode: 'client' },
     // '~/plugins/api.ts',
   ],
