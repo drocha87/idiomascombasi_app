fb2bd3267f9d406c55fded937189ae734da26768
Diego Rocha <dlmrocha87@gmail.com>
2021-05-26 09:52:29 -0300

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer
+    class="
+      bg-fafa
+      w-full
+      h-8
+      border-t
+      flex
+      items-center
+      text-gray-900
+      px-8
+      text-xs
+      font-ember font-light
+      tracking-wide
+    "
+  >
+    <div class="flex-grow">Feedback</div>
+    <div>© 2021, Basilia Santiago. All rights reserved.</div>
+    <div class="ml-8">
+      <a href="https://idiomascombasi.com.br/legal">Terms of use</a>
+    </div>
+  </footer>
+</template>
