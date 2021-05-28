3f00839845e6c1cf79de2782dc87979d0731441d
Diego Rocha <dlmrocha87@gmail.com>
2021-05-28 18:26:40 -0300

    fix Resource Info component when url is not passed

--- components/Resource/Info.vue
+++ components/Resource/Info.vue
@@ -1,18 +1,25 @@
 <template>
-  <a
-    class="block border rounded p-2 text-xs"
-    v-bind="$attrs"
-    :href="to"
-    target="_blank"
-  >
-    <div class="flex items-center justify-between text-blueaws">
-      <div>{{ label }}</div>
-      <div class="uppercase">{{ type }}</div>
-    </div>
-    <div v-if="description !== ''" class="font-ember mt-1 text-gray-700">
-      {{ description }}
-    </div>
-  </a>
+  <div class="block border rounded p-2 text-xs font-medium">
+    <a v-if="hasUrl" v-bind="$attrs" :href="to" target="'_blank'">
+      <div class="flex items-center justify-between text-blueaws">
+        <div>{{ label }}</div>
+        <div class="uppercase">{{ type }}</div>
+      </div>
+      <div v-if="description !== ''" class="font-ember mt-1 text-gray-700">
+        {{ description }}
+      </div>
+    </a>
+    <span v-else v-bind="$attrs">
+      <div class="flex items-center justify-between text-blueaws">
+        <div>{{ label }}</div>
+        <div class="uppercase">{{ type }}</div>
+      </div>
+      <div v-if="description !== ''" class="font-ember mt-1 text-gray-700">
+        {{ description }}
+      </div>
+    </span>
+    <slot name="action"></slot>
+  </div>
 </template>
 
 <script lang="ts">
@@ -25,6 +32,12 @@ export default Vue.extend({
     type: { type: String, default: 'url' },
     description: { type: String, default: '' },
     to: { type: String, required: true },
+  },
+
+  computed: {
+    hasUrl(): boolean {
+      return this.to && this.to.length > 0
+    },
   },
 })
 </script>
