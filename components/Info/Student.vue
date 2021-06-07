7656f07cda13e7428ec98f1cd2f863b073dc46b2
Diego Rocha <dlmrocha87@gmail.com>
2021-06-07 10:39:05 -0300

    add student search

---
+++ components/Info/Student.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="block border rounded p-2 text-xs font-medium">
+    <nuxt-link v-bind="$attrs" :to="`/admin/students/${student.id}`">
+      <div class="flex items-center justify-between text-blueaws">
+        <div>
+          <StudentNickname :student="student" />
+        </div>
+      </div>
+    </nuxt-link>
+    <slot name="action"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { User } from '@/types'
+
+export default Vue.extend({
+  inheritAttrs: false,
+
+  props: {
+    student: { type: Object as () => User, required: true },
+  },
+
+  computed: {},
+})
+</script>
--- pages/admin/modules/index.vue
+++ pages/admin/modules/index.vue
@@ -35,28 +35,6 @@
         class="mb-2"
         :mod="mod"
       />
-      <!-- <div v-if="modules.length" class="w-full mt-8">
-        <nuxt-link
-          v-for="(mod, index) in modules"
-          :key="mod.id"
-          class="block py-2 px-4 w-full hover:bg-gray-200"
-          :class="{
-            'bg-gray-100': index % 2 === 0,
-            'bg-gray-50': index % 2 !== 0,
-          }"
-          :to="mod.id"
-          append
-        >
-          <div class="flex imtes-center justify-between">
-            <div>
-              <span class="text-sm">{{ mod.title }}</span>
-            </div>
-            <div class="capitalize text-xs p-2 bg-green-100 px-4 select-none">
-              {{ mod.language }}
-            </div>
-          </div>
-        </nuxt-link>
-      </div> -->
     </ContainerSlot>
   </div>
 </template>
