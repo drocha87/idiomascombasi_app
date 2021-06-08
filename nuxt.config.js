a72312c5367826e78321229537d49a77ec9000e0
Diego Rocha <dlmrocha87@gmail.com>
2021-06-08 09:54:44 -0300

    add title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'idiomascombasi_app',
+    title: 'Campus ICB',
     htmlAttrs: {
       lang: 'en',
     },
