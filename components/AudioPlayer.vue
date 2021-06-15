76181b25dcfc8232c82e0c7a112da5f15d357212
Diego Rocha <dlmrocha87@gmail.com>
2021-06-15 16:46:24 -0300

    add audio and video player components

---
+++ components/AudioPlayer.vue
@@ -0,0 +1,23 @@
+<template>
+  <audio controls controlsList="nodownload" @play.once="play">
+    <source :src="source" v-bind="$attrs" />
+    Your browser does not support the audio element.
+  </audio>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  inheritAttrs: false,
+  props: {
+    source: { type: String, required: true },
+  },
+
+  methods: {
+    play() {
+      // TODO: implement counter and set as visualized
+      console.log('clicked in play')
+    },
+  },
+})
+</script>
---
+++ components/VideoPlayer.vue
@@ -0,0 +1,18 @@
+<template>
+  <video :width="width" :height="height" controls controlsList="nodownload">
+    <source :src="source" v-bind="$attrs" />
+    Your browser does not support the video element.
+  </video>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  inheritAttrs: false,
+  props: {
+    source: { type: String, required: true },
+    width: { type: String, default: '320' },
+    height: { type: String, default: '240' },
+  },
+})
+</script>
