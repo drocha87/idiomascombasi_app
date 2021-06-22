1c3f0fccdb07a458a8f45bc1bd6cd33888a40b13
Diego Rocha <dlmrocha87@gmail.com>
2021-06-22 16:00:44 -0300

    add roles

---
+++ store/admin/roles.ts
@@ -0,0 +1,35 @@
+import { GetterTree, ActionTree, MutationTree } from 'vuex'
+import { Role } from '@/types'
+
+export const state = () => {
+  const roles: Role[] = []
+
+  return {
+    roles,
+  }
+}
+
+export type RootState = ReturnType<typeof state>
+
+export const getters: GetterTree<RootState, RootState> = {
+  roles(state): Role[] {
+    return state.roles
+  },
+}
+
+export const mutations: MutationTree<RootState> = {
+  SET_ROLES(state, roles: Role[]): void {
+    state.roles = roles
+  },
+}
+
+export const actions: ActionTree<RootState, RootState> = {
+  async fetchRoles({ commit }) {
+    try {
+      const { roles } = await this.$adminapi.$get('roles/')
+      commit('SET_ROLES', roles)
+    } catch (error) {
+      commit('info/SET_ERROR', error, { root: true })
+    }
+  },
+}
